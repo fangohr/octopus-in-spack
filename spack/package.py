e9f6c51237e18336a6098ebf8f595eff429f9872
Hans Fangohr <fangohr@users.noreply.github.com>
2023-10-27 15:08:09 +0200

    Input from Ashwin
    Co-authored-by: Ashwin Kumar Karnad <46030335+iamashwin99@users.noreply.github.com>

--- spack/package.py
+++ spack/package.py
@@ -128,7 +128,7 @@ class Octopus(AutotoolsPackage, CudaPackage):
     depends_on("libyaml", when="+libyaml")
     depends_on("pnfft", when="+pnfft")
     depends_on("nlopt", when="+nlopt")
-    depends_on("dftbplus~mpi", when="+dftbplus")
+    depends_on("dftbplus~mpi+api+sharedlibs~openmp", when="+dftbplus")
 
     # optional dependencies:
     # TODO: etsf-io, sparskit,
